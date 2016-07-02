bb1509720a51522341db9078c07431a2ce55ab45
sizee <994043269@qq.com>
2016-07-02 12:41:19 +0800

    task1整理

---
+++ task1.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Fly-mydream on 16.6.30.
+ */
+
+    var colors;
+    colors = ["red", "green", "blue"];
+    var numbers;
+    numbers=[0,1,2,3,4,5,6,7,8];
+     var i;
+    function changeColor(){
+        var aaa,bbb;
+        aaa = colors[Math.floor(Math.random() * 3)];
+        bbb=numbers[Math.floor(Math.random() * 9)];
+        for( i=0;i<9;i++) {
+            document.getElementById("color-"+i).style.backgroundColor="#e8830d";
+        }
+        if(i==8) {
+            i=0;
+        }
+        document.getElementById("color-"+bbb).style.backgroundColor=aaa;
+      }
+     function func(){
+         setInterval("changeColor()",1000);
+     }
